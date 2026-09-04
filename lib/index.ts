f07ed8c061bee3c445563fe0b971f98a9827ebe8
Simon Kadisch <simon.kadisch@k13-engineering.at>
2026-09-04 15:23:14 +0200

    Export basic types

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,14 @@
 import { createDefaultDmabufHeapManager } from "./dmabuf-heap/index.ts";
 import { createDefaultUdmabufAllocator } from "./udmabuf/index.ts";
+import type { TDmabufHeapManager } from "./dmabuf-heap/dmabuf-heap-manager.ts";
+import type { TDmabufAllocator } from "./dmabuf-allocator-interface.ts";
 
 export {
   createDefaultDmabufHeapManager,
   createDefaultUdmabufAllocator
+};
+
+export type {
+  TDmabufHeapManager,
+  TDmabufAllocator
 };
